75ad77420a7f33e95779a7b7a3e242ff72f5d014
jefersonla <oi_je@hotmail.com>
2015-11-07 17:47:38 -0300

    Finshed version 0.4, works, but need to be better tested

--- src/MideaIR.h
+++ src/MideaIR.h
@@ -1,10 +1,10 @@
 /* 
-	Author: 	Jeferson Lima
-	Date: 		2015-11-05
-	License: 	GNU GPL
+    Author:     Jeferson Lima
+    Date:       2015-11-05
+    License:    GNU GPL
 
-	*** Developed at WiserUFBA Research Group *** 
-						***
+    *** Developed at WiserUFBA Research Group *** 
+                        ***
 */
 #ifndef MideaIR_h
 #define MideaIR_h
@@ -12,45 +12,92 @@
 #include <stdint.h>
 #include <IRremote.h>
 
+
+/*  fan_speed   = 1110: off     1011: auto;   1001: 1,      0101: 2,      0011: 3
+    command     = 0000: cool,   1000: auto,   1100: heat,   1101: humidity extraction */
+enum Modes{
+    mode_cool        = 0x0,
+    mode_auto        = 0x8,
+    mode_heat        = 0xC,
+    mode_no_humidity = 0xD
+};
+
+enum FanSpeed{
+    fan_off     = 0xE,
+    fan_auto    = 0xB,
+    fan_speed_1 = 0x9,
+    fan_speed_2 = 0x5,
+    fan_speed_3 = 0x3
+}
+
+// Midea IR Times
+#define MIDEA_MARK_LIMIT    4200
+#define MIDEA_MARK_LIMIT2   550
+#define MIDEA_SPACE_LIMIT   4500
+#define MIDEA_MARK          450
+#define MIDEA_SPACE         1700
+#define MIDEA_SPACE2        600
+
+// Midea IR Frequency
+#define MIDEA_FREQUENCY     38
+
+// Midea IR Codes
+#define MIDEA_START_BYTE    0xB2
+
+// Bytes to turn off Midea Air Conditioner
+#define STOP_MIDEA_BYTE1    0x7B
+#define STOP_MIDEA_BYTE2    0xE0
+
 class MideaIR{
 private:
-	/* Basic Functions */
+    /* Basic Functions */
     uint8_t mode;
     uint8_t temperature;
     uint8_t speed_fan;
-	bool 	state;
+    bool    state;
     /* Complex Feature */
-    bool 	osci_dir;
+    bool    osci_dir;
     /* Extra Functions */
-    bool	sleep;
-    bool 	turbo;
-    bool 	sound;
+    bool    sleep;
+    bool    turbo;
+    bool    sound;
     /* IR Emitter Object */
-    IRsend 	*irsend;
+    IRsend  *irsend;
 
     /* Control Bytes */
     uint8_t command_byte1;
     uint8_t command_byte2;
-    uint8_t command_byte3;
-    uint8_t command_byte4;
+
+    /* Low Level Operations */
+    void    loWLevelEmit();
+    void    generateCommand();
+    void    emitByte(uint8_t);
 public:
-	// Construct
-	MideaIR(IRsend);
+    // Construct
+    MideaIR(IRsend *);
 
-	/* Basic Functions */
-	// Change Values
-	void 	setMode(char);
-	void 	setState(bool);
-	void 	setSpeedFan(int);
-	void 	setTemperature(int);
-	// Retrieve Values
-	char 	getMode();
-	uint8_t	getState();
-	uint8_t getSpeedFan();
-	uint8_t getTemperature();
+    /* Basic Functions */
+    void    turnON();
+    void    turnOFF();
 
-	// Emit the code
-	void 	emmit();
+    /* Class Based Functions */
+    // Change Values
+    void    setMode(uint8_t);
+    void    setState(bool);
+    void    setSpeedFan(int);
+    void    setTemperature(int);
+    // Retrieve Values
+    uint8_t getMode();
+    bool    getState();
+    uint8_t getSpeedFan();
+    uint8_t getTemperature();
+
+    /* Complex Buttons */
+    void    doOscilate();
+    void    doChangeDirection();
+
+    // Emmit the command
+    void    emit();
 };
 
 #endif
--- src/MidearIR.cpp
+++ src/MidearIR.cpp
@@ -1,8 +1,205 @@
 /* 
-	Author: 	Jeferson Lima
-	Date: 		2015-11-05
-	License: 	GNU GPL
+    Author:     Jeferson Lima
+    Date:       2015-11-05
+    License:    GNU GPL
 
-	*** Developed at WiserUFBA Research Group *** 
-						***
+    *** Developed at WiserUFBA Research Group *** 
+                        ***
 */
+#include <stdint.h>
+#include <IRremote.h>
+
+MideaIR::MideaIR(IRsend *ref){
+    irsend = ref;
+}
+
+/* Class Based Functions */
+// Change Values
+void MideaIR::setState(bool new_state){
+    state = new_state;
+}
+
+void MideaIR::setMode(uint8_t new_mode){
+    switch(new_mode){
+        case mode_cool       :
+        case mode_auto       :
+        case mode_heat       :
+        case mode_no_humidity:
+            mode = new_mode; break;
+        default:
+            mode = mode_auto;
+    }
+}
+
+void MideaIR::setSpeedFan(int new_speed_fan){
+    switch(new_speed_fan){
+        case fan_off    : 
+        case fan_auto   :
+        case fan_speed_1:
+        case fan_speed_2:
+        case fan_speed_3:
+            speed_fan = new_speed_fan; break;
+        default: 
+            speed_fan = fan_auto;
+    }
+}
+
+void MideaIR::setTemperature(int new_temperature){
+    if(new_temperature > 30)
+        temperature = 30;
+    else if(new_temperature < 17)
+        temperature = 17;
+    else
+        temperature = new_temperature;
+}
+
+// Retrieve Valuestemperature
+uint8_t MideaIR::getMode(){
+    return mode;
+}
+
+bool MideaIR::getState(){
+    return state;
+}
+
+uint8_t MideaIR::getSpeedFan(){
+    return speed_fan;
+}
+
+uint8_t MideaIR::getTemperature(){
+    return temperature;
+}
+
+// Generate command bytes
+void MideaIR::generateCommand(){
+    /*  The Midea Air Conditioner is controlled by two bytes that defines
+        all comands in the controller. Basically what we have is the following
+        mask: [1011 0010] [ffff 1111] [ttttcccc]
+        As pointed by Daniel Veillard in the post http://veillard.com/embedded/midea.html
+        Daniel said that we don't understand some values like temperature, for this we 
+        had expanded his research. First he have understand how temperature is definied,
+        and is basically 17 - temperature you want and then convert this to grey code,
+        really strange for a programmer, but make sense at embedded technologies, another
+        thing that we discovered is how perfom some "complex features" like, night mode,
+        turbo mode, ionize mode, sleep mode (I think they are different in something that
+        I don't care) and other functions that sucks '-_-.
+        The mask that we use here is the following:
+         START BYTE   FIRST BYTE   LAST BYTE
+        [1011  0010] [ssss  ffff] [tttt mmmm]
+        - ssss = Fan Speed
+        - ffff = Features
+        - tttt = Temperature
+        - mmmm = Mode
+    */
+
+    uint8_t b_mode;
+    uint8_t b_feature;
+    uint8_t b_temperature;
+    uint8_t b_fan_speed;
+    uint8_t aux;
+
+    // If the logic state of the Air Conditioner is off, turn off
+    if(!state){
+        command_byte1 = STOP_MIDEA_BYTE1;
+        command_byte2 = STOP_MIDEA_BYTE2;
+        return;
+    }
+
+    // Prepare Temperature
+    aux = temperature - 17; // Need to check if the numers 27 and 28 work with this
+    aux = (aux >> 1) ^ aux; // -> Convert decimal into grey code
+    b_temperature = aux;
+
+    // Prepare Mode and Fan Speed are alreay prepared so just copy the values to the temp var
+    b_mode      = mode; 
+    b_fan_speed = fan_speed;
+
+    // Prepare Features
+    b_feature = 0xF; // <- Not implemented yet
+
+    // Assembly First Byte
+    command_byte1 = (b_fan_speed << 4)   | (b_feature);
+
+    // Assembly Last Byte
+    command_byte2 = (b_temperature << 4) | (b_mode);
+}
+
+void MideaIR::emitByte(uint8_t byte_to_send){
+    // Thanks to Daniel Veillard, for this incredible function :D
+    // And thanks to Tom to help us with this work
+    // Tom Topic about Midea : 
+    // http://thebloughs.net/design-a-custom-ac-timer/
+    int i;
+
+    // Pick the current byte, and his mask
+    uint8_t cur = byte_to_send;
+    uint8_t mask = 0x80;
+
+    // Emit the first Byte
+    for (i = 0;i < 8;i++) {
+        irsend.mark(MIDEA_MARK);
+        if (cur & mask)
+            irsend.space(MIDEA_SPACE);
+        else
+            irsend.space(MIDEA_SPACE2);
+        mask >>= 1;
+    }
+
+    // Neg bitwise this byte with his mask
+    cur = ~byte_to_send;
+    mask = 0x80;
+
+    // And emit the same byte negate
+    for (i = 0;i < 8;i++) {
+        irsend.mark(MIDEA_MARK);
+        if (cur & mask)
+            irsend.space(MIDEA_SPACE);
+        else
+            irsend.space(MIDEA_SPACE2);
+        mask >>= 1;
+    }
+}
+
+void MideaIR::loWLevelEmit(){
+    // Start Communication
+    irsend.mark(MIDEA_MARK_LIMIT);
+    irsend.space(MIDEA_SPACE_LIMIT);
+    
+    // Emit Some bytes
+    emitByte(MIDEA_START_BYTE);
+    emitByte(command_byte1);
+    emitByte(command_byte2);
+
+    // End Communication
+    irsend.mark(MIDEA_MARK_LIMIT2);
+    irsend.space(MIDEA_SPACE_LIMIT);
+}
+
+// Emit the command
+void MideaIR::emit(){
+    // Prepare the command bytes
+    generateCommand();
+
+    // Set IR Configuration
+    irsend.enableIROut(MIDEA_FREQUENCY);
+
+    // Send two times the same command, we have not discovered why this is necessary :?
+    loWLevelEmit();
+    loWLevelEmit();
+}
+
+/* Basic Functions */
+void MideaIR::turnON(){
+    state = true;
+    emit();
+}
+
+void MideaIR::turnOFF(){
+    state = false;
+    emit();
+}
+
+/* Complex Buttons */
+void MideaIR::doOscilate(){} // <~ Not implemented yet
+
+void MideaIR::doChangeDirection(){} // Not implemented yet
