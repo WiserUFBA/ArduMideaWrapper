67362c5e6df7edeeedb12e43514a118ff2a7a9c1
Andressa Andrade <dsandrade@dcc.ufba.br>
2016-03-22 11:43:26 -0300

    Update use_explaned.ino

--- examples/use_explaned/use_explaned.ino
+++ examples/use_explaned/use_explaned.ino
@@ -1,6 +1,7 @@
 #include <IRremote.h>
 #include <MideaIR.h>
 #define IR_EMITER 3
+#define FIVE_SECONDS    5000
 
 // IRsend Object and Remote Control object
 IRsend irsend;
@@ -10,7 +11,34 @@ void setup(){
   // Define IR PIN as Output
   pinMode(IR_EMITER, OUTPUT);
 
-  // Do Unit tests
+  // Try to turn ON the Air Conditioner
+  Serial.println(F("Trying to Turn ON the Air Conditioner..."));
+  remote_control.turnON();
+   delay(FIVE_SECONDS);
+
+  // Try to turn OFF
+  Serial.println(F("Trying to Turn OFF the Air Conditioner..."));
+  remote_control.turnOFF();
+  delay(FIVE_SECONDS);
+  
+  // Change mode
+  remote_control.setState(true);
+
+  // Change mode
+  remote_control.setMode(mode_auto);
+  remote_control.emit();
+  delay(FIVE_SECONDS);
+  
+  // Change speed fan
+  remote_control.setSpeedFan(fan_speed_1);
+  remote_control.emit();
+  delay(FIVE_SECONDS);
+  
+  // Change temperature to 22
+  remote_control.setTemperature(22);
+  remote_control.emit();
+  delay(FIVE_SECONDS);
+  
 }
 
 void loop(){}
